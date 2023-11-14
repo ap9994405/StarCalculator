bd07ebf2ec0e645ee23a5901035542529bf14eee
ap9994405 <150656530+ap9994405@users.noreply.github.com>
2023-11-14 14:19:43 +0800

    Restore file to origin input.f90

--- src/input.f90
+++ src/input.f90
@@ -49,10 +49,9 @@ subroutine read_input(input_fname,pah)
         end do
       end if
     end do
-    allocate(pah%initiallabel(cnat))
-    call sort(geom,cnat,pah%initiallabel)
+    if (.not. unsorted_geometry) call sort(geom,cnat)
   end if ! is_adjacency_file
-  
+
 ! ######################################
 ! # verify the maximal number of atoms #
 ! ######################################
@@ -69,6 +68,7 @@ subroutine read_input(input_fname,pah)
 ! ######################
   pah%nat=cnat
   pah%order=0
+  allocate(pah%initiallabel(pah%nat))
   allocate(pah%neighbornumber(pah%nat))
   allocate(pah%neighborlist(3,pah%nat))
   pah%neighbornumber=0
@@ -118,8 +118,6 @@ subroutine read_input(input_fname,pah)
         read (20, *) nConnection
         do i=1, nConnection
             read (20,*) con1, con2
-            con1 = pah%initiallabel(con1)
-            con2 = pah%initiallabel(con2)
             if (dist(cnat,con1,con2,geom) < ccdist) then
                 pah%neighbornumber(con1)=pah%neighbornumber(con1)+1
                 pah%neighborlist(pah%neighbornumber(con1),con1)=con2
