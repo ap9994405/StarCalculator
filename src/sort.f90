6c751cfb28284ec1d69507e8ee5d529a9828dcb9
ap9994405 <150656530+ap9994405@users.noreply.github.com>
2023-11-14 14:20:18 +0800

    Update sort.f90
    Restore file to origin

--- src/sort.f90
+++ src/sort.f90
@@ -1,39 +1,22 @@
 ! Simple insertion sort of an XYZ array
-subroutine sort(a,n,initiallabel)
+subroutine sort(a,n)
   use types_module
   implicit none
   
   integer, intent(in) :: n
   real(kreal), intent(inout) :: a(3,n)
-  integer, intent(inout) :: initiallabel(n)
-  integer :: tmpinitiallabel(n)
-  real(kreal) :: tmp(3),tmplabel
+  real(kreal) :: tmp(3)
   integer :: i,j
-  do i=1,n
-    initiallabel(i)=i
-  end do
 
   do i=2,n
     tmp=a(:,i)
-    tmplabel=(i)
     j=i-1
     do while (j>=1) ! equivalent to sort -k 3,3g -k 2,2g -k 1,1g
       if (a(3,j) < tmp(3) .or.(a(3,j) == tmp(3) .and. a(2,j) < tmp(2)) .or. &
              ( a(3,j) == tmp(3) .and. a(2,j) == tmp(2) .and. a(1,j) < tmp(1))) exit 
       a(:,j+1)=a(:,j)
-      initiallabel(j+1)=initiallabel(j)
       j=j-1
     end do
     a(:,j+1)=tmp
-    initiallabel(j+1)=tmplabel
   end do
-  do i=1,n
-    do j=1,n
-      if (initiallabel(j)==i) then
-        tmpinitiallabel(i)=j
-        exit
-      end if 
-    end do
-  end do
-  initiallabel = tmpinitiallabel 
 end subroutine
